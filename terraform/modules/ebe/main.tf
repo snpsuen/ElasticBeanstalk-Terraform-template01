2bb8b142b1165b64a6679409a38925c48cee4654
snpsuen <66404197+snpsuen@users.noreply.github.com>
2020-09-12 17:11:02 +0800

    Update main.tf

--- terraform/modules/ebe/main.tf
+++ terraform/modules/ebe/main.tf
@@ -1,5 +1,5 @@
 module "label" {
-  source      = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.17.0"
+  source      = "../label/"
   namespace   = var.namespace
   environment = var.environment
   name        = var.name
@@ -936,7 +936,7 @@ resource "aws_s3_bucket" "elb_logs" {
 }
 
 module "dns_hostname" {
-  source  = "git::https://github.com/cloudposse/terraform-aws-route53-cluster-hostname.git?ref=tags/0.5.0"
+  source  = "../dnshost/"
   enabled = var.dns_zone_id != "" && var.tier == "WebServer" ? true : false
   name    = var.dns_subdomain != "" ? var.dns_subdomain : var.name
   zone_id = var.dns_zone_id
