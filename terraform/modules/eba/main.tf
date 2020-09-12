e360ce36a7e734c71c486cb035f736e051654722
snpsuen <66404197+snpsuen@users.noreply.github.com>
2020-09-12 17:30:42 +0800

    Create main.tf

---
+++ terraform/modules/eba/main.tf
@@ -0,0 +1,33 @@
+module "label" {
+  source      = "../label/"
+  namespace   = var.namespace
+  environment = var.environment
+  name        = var.name
+  stage       = var.stage
+  delimiter   = var.delimiter
+  attributes  = var.attributes
+  tags        = var.tags
+}
+
+locals {
+  // Remove `Name` tag from the map of tags because Elastic Beanstalk generates the `Name` tag automatically
+  // and if it is provided, terraform tries to recreate the application on each `plan/apply`
+  // `Namespace` should be removed as well since any string that contains `Name` forces recreation
+  // https://github.com/terraform-providers/terraform-provider-aws/issues/3963
+  tags = { for t in keys(module.label.tags) : t => module.label.tags[t] if t != "Name" && t != "Namespace" }
+}
+
+resource "aws_elastic_beanstalk_application" "default" {
+  name        = module.label.id
+  description = var.description
+  tags        = local.tags
+
+  dynamic "appversion_lifecycle" {
+    for_each = var.appversion_lifecycle_service_role_arn != "" ? ["true"] : []
+    content {
+      service_role          = var.appversion_lifecycle_service_role_arn
+      max_count             = var.appversion_lifecycle_max_count
+      delete_source_from_s3 = var.appversion_lifecycle_delete_source_from_s3
+    }
+  }
+}
